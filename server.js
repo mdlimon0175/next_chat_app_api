f7077506f95998c601f4a00eabddd53dd722daf1
KiamKhan <mdlimon0175@gmail.com>
2025-10-31 04:53:55 +0600

    update: mongoose connect before listening server.

--- server.js
+++ server.js
@@ -14,6 +14,22 @@ const DatabaseSeeder = require('./database/seeders/DatabaseSeeder');
 // create server
 const server = createServer(app);
 
+mongoose.connect(appConfig.databaseURL).then(fullfill => {
+    console.log("database connected!");
+    const runSeeder = process.argv.findIndex(argv => argv === 'db:seed') !== -1;
+    if(process.env.NODE_ENV === 'production' && runSeeder) {
+        DatabaseSeeder.run().then(result => {
+            if(result) {
+                console.log('Seed successful!');
+            } else {
+                console.log('Failed to seed data');
+            }
+        })
+    }
+}).catch(err => {
+    console.log(err);
+});
+
 // socket.io setup
 const io = new SocketServer(server, {
     cors: {
@@ -27,19 +43,4 @@ io.on('connection', (socket) => {
 
 server.listen(appConfig.port, () => {
     console.log(`Server running on port - ${appConfig.port}`);
-    mongoose.connect(appConfig.databaseURL).then(fullfill => {
-        console.log("database connected!");
-        const runSeeder = process.argv.findIndex(argv => argv === 'db:seed') !== -1;
-        if(process.env.NODE_ENV === 'production' && runSeeder) {
-            DatabaseSeeder.run().then(result => {
-                if(result) {
-                    console.log('Seed successful!');
-                } else {
-                    console.log('Failed to seed data');
-                }
-            })
-        }
-    }).catch(err => {
-        console.log(err);
-    });
 });
