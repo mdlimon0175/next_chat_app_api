d4a4208657690129494448a4aa38081c2997ac9c
KiamKhan <mdlimon0175@gmail.com>
2025-11-11 18:12:54 +0600

    fix: conversation random creator create conversation multiple times

--- database/seeders/ConversationSeeder.js
+++ database/seeders/ConversationSeeder.js
@@ -6,7 +6,6 @@ class ConversationSeeder {
         const conversations = await ConversationFactory.getConversation();
         if(!Array.isArray(conversations)) {
             const existingConversationData = await Conversation.findOne({
-                created_by: conversations.created_by,
                 participants: { $all: conversations.participants }
             });
 
