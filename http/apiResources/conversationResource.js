801be39a3050298346257f5d6a76c5e6bc793577
KiamKhan <mdlimon0175@gmail.com>
2025-11-11 19:22:35 +0600

    fix: error solve for conversation.last_message. cause initially last_message is null

--- http/apiResources/conversationResource.js
+++ http/apiResources/conversationResource.js
@@ -4,12 +4,12 @@ function conversationResource(data) {
     const prepareConversations = (c) => ({
         id: c._id,
         participants: prepareChatParticipantsInfo(c.participants),
-        last_message: {
+        last_message: Object.hasOwn(c, 'last_message') ? {
             id: c.last_message._id,
             sender_id: c.last_message.sender_id,
             message: c.last_message.message,
             sent_at: c.last_message.sent_at,
-        },
+        } : null,
     });
 
     return Array.isArray(data) ? data.map(prepareConversations) : prepareConversations(data);
